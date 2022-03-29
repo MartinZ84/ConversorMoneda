96c6140fc83c84a458277eee8047208bc31dd738
Martin Zanche <martinzanche@gmail.com>
2022-03-29 20:25:47 -0300

    menu sin terminar

--- app/src/main/java/com/devmhz/conversormoneda/MainActivity.java
+++ app/src/main/java/com/devmhz/conversormoneda/MainActivity.java
@@ -1,8 +1,11 @@
 package com.devmhz.conversormoneda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.dolar){
+            //Hacer algo
+            return true;
+
+        }
+        return true;
+
     }
 }
